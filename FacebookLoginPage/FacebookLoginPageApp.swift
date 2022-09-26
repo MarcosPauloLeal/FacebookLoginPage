dc201565d205d452c0e735d1e03ed248d2f8952f
Marcos Paulo Leal <marcospaulolealsilva@icloud.com>
2022-09-26 20:26:10 -0300

    Initial Commit

---
+++ FacebookLoginPage/FacebookLoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginPageApp.swift
+//  FacebookLoginPage
+//
+//  Created by Marcos Paulo Leal Silva on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+- Facebook Login Page - SwiftUI - Instructor: Giovanna Moeller.
+
+<div align="center">
+    <img src="img/screenshot.png" width="400px"</img> 
+</div>
+
